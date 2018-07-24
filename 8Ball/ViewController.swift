8095b304ee34467dc1182b2b44f70163b42a5a45
Lisa Sampson <lisasampson0918@gmail.com>
2018-07-24 12:59:32 -0400

    Initial commit

--- 8Ball/ViewController.swift
+++ 8Ball/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func flipEightBall(_ sender: Any) {
+        
+        let index = Int(arc4random_uniform(UInt32(answers.count)))
+        let answer = answers[index]
+        answerLabel.text = answer
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    private let answers = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no",
+        "Outlook not so good.",
+        "Very doubtful.",
+        ]
 }
 
